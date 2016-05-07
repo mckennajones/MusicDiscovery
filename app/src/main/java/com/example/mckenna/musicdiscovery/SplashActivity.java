b7d20f630d6e405ae8c939c5d4fc2998c1cf3abe
McKenna Jones <mjones0014@gmail.com>
2016-05-06 21:31:19 -0700

    Added splash screen

---
+++ app/src/main/java/com/example/mckenna/musicdiscovery/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.mckenna.musicdiscovery;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
+
+/**
+ * Created by mckenna on 5/6/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        SystemClock.sleep(1000);
+        finish();
+    }
+}
